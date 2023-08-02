3ddea8ff80c36e6beec200fb300a01a737a933a1
Ksenia Shurina <kseniashurina18@gmail.com>
2023-08-02 17:24:23 +0200

    Added Controller to update data in a product XSTR-11

---
+++ MobileStore.Presentation/Controllers/ProductsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MobileStore.Core.Abstractions.Services;
+using MobileStore.Presentation.Controllers.Base;
+using MobileStore.Presentation.ViewModels;
+
+namespace MobileStore.Presentation.Controllers
+{
+    [Route("{controller}")]
+    public class ProductsController : MvcControllerBaseSecure
+    {
+        private readonly IProductService _productService;
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet("{productId}/{action}")]
+        public async Task<IActionResult> Edit([FromRoute] Guid productId)
+        {
+            var product = await _productService.GetProduct(productId);
+            var editProductViewModel = new EditProductViewModel
+            {
+               Id = product.Id,
+               ProductTypeId = product.ProductTypeId,
+               Name = product.Name,
+               Company = product.Company,
+               Price = product.Price,
+               Img = product.Img,
+            };
+            return View(editProductViewModel);
+        }
+    }
+}
---
+++ MobileStore.Presentation/ViewModels/EditProductViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace MobileStore.Presentation.ViewModels
+{
+    public class EditProductViewModel
+    {
+        public Guid Id { get; init; }
+        public Guid ProductTypeId { get; init; }
+        public string Name { get; init; } = null!;
+        public string Company { get; init; } = null!;
+        public double Price { get; init; }
+        public string Img { get; init; } = null!;
+    }
+}
