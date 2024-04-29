f80fdfddc2fac2ec4f6ab8f4d452a4be98cef14f
Ksenia Shurina <kseniashurina18@gmail.com>
2024-04-29 21:40:30 +0200

    Migration was added

---
+++ MobileStore.Infrastructure/Migrations/20240429193924_AddingPropertiesToEntities.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MobileStore.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddingPropertiesToEntities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Img",
+                table: "Products");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Img",
+                table: "Products",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
